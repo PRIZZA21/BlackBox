9788c9f5552678470e8471462a7f64c6efbbaadc
bipangshu <bumba8812@gmail.com>
2025-11-01 18:42:35 +0530

    Vector conversion for RAG

---
+++ Desktop/Web-Development/e-cell-website-22-newdev/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import GalleryPage from './pages/GalleryPage/GalleryPage';
+import Pillars from './components/Pillars of Ecell/Pillars';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={< HomePage />} key='route-home-screen' />
+            <Route exact path="/gallery" element={< GalleryPage />} key='route-gallery-screen' />
+          </Routes>
+      </BrowserRouter>
+      <Pillars />
+  </>
+  );
+}
+
+export default App;
---
+++ Desktop/Web-Development/e-cell-website-22-newdev/src/pages/GalleryPage/GalleryPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import GalleryHeader from '../../components/Gallery/GalleryHeader/GalleryHeader'
+
+function GalleryPage() {
+  return (
+    <div><GalleryHeader /></div>
+  )
+}
+
+export default GalleryPage
