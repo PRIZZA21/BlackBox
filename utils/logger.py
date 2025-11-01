97ffcb8e09fde9364a87418e6c421e7f271cebab
Priyanshu <priyanshujayant3121@gmail.com>
2025-11-01 18:11:54 +0530

    Remove app.log from git tracking

--- utils/logger.py
+++ utils/logger.py
@@ -1,7 +1,14 @@
 import logging
 import sys
 
-# Configure logging
+# Suppress chromadb and other noisy loggers
+logging.getLogger("chromadb").setLevel(logging.CRITICAL)
+logging.getLogger("chromadb.telemetry").setLevel(logging.CRITICAL)
+logging.getLogger("chromadb.telemetry.posthog").setLevel(logging.CRITICAL)
+logging.getLogger("posthog").setLevel(logging.CRITICAL)
+logging.getLogger("onnxruntime").setLevel(logging.CRITICAL)
+
+# Configure your app logging
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -12,4 +19,3 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
-
